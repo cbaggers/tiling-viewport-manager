e88385dbf141fbdf1cc4335780341a6777febd93
Baggers <chris.bagley@gmail.com>
2016-09-26 22:19:09 +0200

    spliting and rendering of colors & textures works

--- base.lisp
+++ base.lisp
@@ -2,20 +2,21 @@
 (in-readtable :fn.reader)
 
 (defparameter *minimum-frame-size* 5s0)
+(defparameter *frame-size-px* 5s0)
 
 ;;------------------------------------------------------------
 
-(defparameter *colors* '((V! 0.46 0.46 0.46 0)
-                         (V! 0.26 0.26 0.26 0)
-                         (V! 0.43 0.43 0.43 0)
-                         (V! 0.37 0.37 0.37 0)
-                         (V! 0.17 0.17 0.17 0)
-                         (V! 0.2 0.2 0.2 0)
-                         (V! 0.29 0.29 0.29 0)
-                         (V! 0.31 0.31 0.31 0)
-                         (V! 0.4 0.4 0.4 0)
-                         (V! 0.34 0.34 0.34 0)
-                         (V! 0.23 0.23 0.23 0)))
+(defparameter *colors* (list (v! 0.46 0.46 0.46 0)
+                             (v! 0.26 0.26 0.26 0)
+                             (v! 0.43 0.43 0.43 0)
+                             (v! 0.37 0.37 0.37 0)
+                             (v! 0.17 0.17 0.17 0)
+                             (v! 0.2 0.2 0.2 0)
+                             (v! 0.29 0.29 0.29 0)
+                             (v! 0.31 0.31 0.31 0)
+                             (v! 0.4 0.4 0.4 0)
+                             (v! 0.34 0.34 0.34 0)
+                             (v! 0.23 0.23 0.23 0)))
 
 (let ((i 0))
   (defun get-col ()
@@ -24,7 +25,8 @@
 
 ;;------------------------------------------------------------
 
-(deftclass (buffer (:conc-name nil) (:constructor %make-buffer))
+(deftclass (buffer (:conc-name nil) (:constructor %%make-buffer))
+  (name (error "A buffer needs a name"))
   (clear-color (get-col))
   (viewport (make-viewport))
   (content nil :type (or null sampler))
@@ -37,10 +39,16 @@
   (intern (format nil "NULL-BUFFER-~a" (incf *buffer-id*)) :keyword))
 
 (defun make-buffer (&optional name)
+  (second (multiple-value-list (%make-buffer name))))
+
+(defun %make-buffer (&optional name frame)
   (assert (or (null name) (keywordp name)))
   (let ((name (or name (gen-null-buffer-name))))
     (assert (not (buffer-exists-p name)))
-    (setf (gethash name *buffers*) (%make-buffer))))
+    (assert (keywordp name))
+    (values (setf (gethash name *buffers*)
+                  (%%make-buffer :name name :frame frame))
+            name)))
 
 (defun kill-buffer (name)
   (assert (buffer-exists-p name))
@@ -63,41 +71,96 @@
              *buffers*)
     (make-buffer)))
 
+(defmethod resolution ((buffer buffer))
+  (with-slots (viewport) buffer
+    (when viewport
+      (viewport-resolution viewport))))
+
+(defmethod (setf resolution) (value (buffer buffer))
+  (with-slots (viewport) buffer
+    (when viewport
+      (setf (viewport-resolution viewport) value))))
+
+(defmethod origin ((buffer buffer))
+  (with-slots (viewport) buffer
+    (when viewport
+      (viewport-origin viewport))))
+
+(defmethod (setf origin) (value (buffer buffer))
+  (with-slots (viewport) buffer
+    (when viewport
+      (setf (viewport-origin viewport) value))))
+
+(defun switch-to-buffer (buffer-name)
+  (assert (buffer-exists-p buffer-name))
+  (%switch-to-buffer (%current-frame) buffer-name))
+
+(defun %switch-to-buffer (frame &optional buffer-name)
+  (with-slots (children) frame
+    (assert (buffer-p children))
+    (let* ((buffer-name (or buffer-name (find-or-make-suitable-buffer)))
+           (buffer (gethash buffer-name *buffers*)))
+      (if (frame buffer)
+          (setf *current-buffer* buffer)
+          (%switch-to-buffer-obj frame buffer))
+      buffer-name)))
+
+(defun %switch-to-buffer-obj (frame buffer)
+  ;; duplicate above asserts for the case when this is called directly
+  (assert buffer)
+  (unless (frame buffer) ;; if buffer already has a frame then just focus
+    (with-slots (children) frame
+      (assert (buffer-p children))
+      (setf children buffer)))
+  (setf *current-buffer* buffer))
+
+(defvar *current-buffer*)
+
+(defun current-buffer ()
+  (name (%current-buffer)))
+
+(defun %current-buffer ()
+  *current-buffer*)
+
+(defun buffer-contents (&optional buffer-name)
+  (if buffer-name
+      (content (gethash buffer-name *buffers*))
+      (content (%current-buffer))))
+
+(defun (setf buffer-contents) (value &optional buffer-name)
+  (assert (or (typep value 'texture)
+              (typep value 'sampler)
+              (null value)))
+  (if buffer-name
+      (setf (content (gethash buffer-name *buffers*)) value)
+      (setf (content (%current-buffer)) value)))
+
+(defun list-all-buffers ()
+  (cepl-utils:map-hash
+   (lambda (k v) (declare (ignore v)) k)
+   *buffers*))
+
 ;;------------------------------------------------------------
 
 (deftclass (frame (:conc-name nil))
   split-type
-  (children (make-buffer)))
+  group-id
+  (children (%make-buffer)))
 
 (defun parent (frame)
-  (unless (find frame *groups*)
-    (find-if #'identity *groups* :key λ(%find-parent _))))
+  (let* ((gid (group-id frame))
+         (root-frame (or (gethash gid *groups*) frame)))
+    (unless (eq frame root-frame)
+      (%find-parent root-frame))))
 
 (defun %find-parent (frame)
   (with-slots (children) frame
     (when (typep children 'list)
       (or (when (find frame children) frame)
-          (find-if #'identity children :key λ(%find-parent _))))))
+          (find-if #'identity children :key λ(%find-parent (first _)))))))
 
-(defvar *current-frame* nil)
-
-(defun %set-current-frame (frame)
-  (setf *current-frame* frame))
-
-(defun switch-to-buffer (buffer-name)
-  (assert (buffer-exists-p buffer-name))
-  (%switch-to-buffer *current-frame* buffer-name))
-
-(defun %switch-to-buffer (frame &optional buffer-name)
-  (with-slots (children) frame
-    (assert (not (listp children)))
-    (let* ((buffer-name (or buffer-name (find-or-make-suitable-buffer)))
-           (buffer (gethash *buffers* buffer-name)))
-      (assert buffer)
-      (setf children buffer))))
-
-(defun focus-buffer-left ()
-  )
+(defun %current-frame ()
+  (frame (gethash (current-buffer) *buffers*)))
 
 ;;------------------------------------------------------------
 
@@ -109,7 +172,7 @@
       (pick-frame-normalize-coords n-x n-y group-id))))
 
 (defun pick-frame-normalize-coords (x y &optional (group-id *current-group*))
-  (let ((frame (elt *groups* group-id)))
+  (let ((frame (gethash group-id *groups*)))
     (%pick-frame frame x y)))
 
 (defun %pick-frame (frame x y)
@@ -131,22 +194,32 @@
 
 ;;------------------------------------------------------------
 
-(defun swap-group (&optional (group-id *current-group*))
-  (render-buffers (elt *groups* group-id))
+(defun swap-group (&optional (group-id (current-group)))
+  (%render-buffers (gethash group-id *groups*))
   (swap))
 
-(defun render-buffers (frame)
+(defun %render-buffers (frame)
   (with-slots (children) frame
     (typecase children
-      (list (map nil #'render-buffers children))
-      (buffer (render-buffer children)))))
+      (list (map nil λ(%render-buffers (first _)) children))
+      (buffer (%render-buffer children)))))
 
-(defun render-buffer (buffer)
+(defun %render-buffer (buffer)
+  (with-viewport (viewport buffer)
+    (with-slots (content clear-color) buffer
+      (typecase content
+        (texture (cepl.misc:draw-texture content nil nil))
+        (sampler (cepl.misc:draw-texture content nil nil))
+        (null (when clear-color
+                (cepl.misc:draw-colored-quad clear-color nil))))))
   (format t "rendering ~s" buffer))
 
 ;;------------------------------------------------------------
 
-(defun split-frame (frame &optional (direction :horizontal))
+(defun split-frame (&optional (direction :horizontal))
+  (%split-frame (%current-frame) direction))
+
+(defun %split-frame (frame &optional (direction :horizontal))
   (assert (find direction '(:horizontal :vertical)))
   (let ((parent (parent frame)))
     (if (and parent (eq direction (split-type parent)))
@@ -157,22 +230,32 @@
 
 (defun %split-frame-children (frame direction)
   (with-slots (children split-type) frame
-    (assert (not (listp children)))
-    (setf children (list (list children 0.5s0 0.5s0)
-                         (list (make-buffer) 0.5s0 0.5s0))
-          split-type direction)
-    (%set-current-frame (first children))))
+    (assert (buffer-p children))
+    (let* ((top-buffer children)
+           (top-frame (make-frame :group-id (group-id frame)
+                                  :children children))
+           (bottom-buffer (%make-buffer))
+           (bottom-frame (make-frame :group-id (group-id frame)
+                                     :children bottom-buffer)))
+      (setf (frame top-buffer) top-frame
+            (frame bottom-buffer) bottom-frame)
+      (setf children (list (list top-frame 0.5s0 0.5s0)
+                           (list bottom-frame 0.5s0 0.5s0))
+            split-type direction)
+      (%switch-to-buffer-obj top-frame top-buffer))))
 
-(defun %split-parent (frame child)
+(defun %split-parent (frame child-to-split)
   (with-slots (children split-type) frame
-    (let ((pos (position child children :key #'first)))
+    (let ((pos (position child-to-split children :key #'first)))
       (destructuring-bind (child w h) (elt children pos)
         (let ((new-w (if (eq split-type :horizontal) (/ w 2s0) w))
               (new-h (if (eq split-type :vertical) (/ h 2s0) h)))
-          (append (subseq children 0 (max 0 (- pos 1)))
-                  (list (list child new-w new-h)
-                        (list (make-frame) new-w new-h))
-                  (subseq children pos)))))))
+          (setf children
+                (append (subseq children 0 (max 0 (- pos 1)))
+                        (list (list child new-w new-h)
+                              (list (make-frame :group-id (group-id frame))
+                                    new-w new-h))
+                        (subseq children pos))))))))
 
 ;;------------------------------------------------------------
 
@@ -180,19 +263,21 @@
   (let ((win cepl.context::*gl-window*))
     (when win
       (destructuring-bind (w h) (cepl.internals:window-size)
-        (mapcar λ(update-frame _ w h 0 0) *groups*))))
+        (update-frame (gethash (current-group) *groups*) w h 0 h))))
   nil)
 
 (defun update-frame (frame w h x y)
   (etypecase frame
-    (viewport
-     (setf (viewport-resolution frame) (v! w h)
-           (viewport-origin frame) (v! x y)))
+    (buffer
+     (setf (resolution frame) (v! w h)
+           (origin frame) (v! x y)))
     (frame
      (with-slots (%-w %-h children) frame
        (etypecase children
-         (viewport (setf (viewport-resolution children) (v! w h)
-                         (viewport-origin children) (v! x y)))
+         (buffer (setf (resolution children) (v! (- w *frame-size-px*)
+                                                 (- h *frame-size-px*))
+                       (origin children) (v! (+ x *frame-size-px*)
+                                             (- y h *frame-size-px*))))
          (list (update-children frame w h x y)))))))
 
 (defun update-children (frame w h x y)
@@ -202,64 +287,85 @@
              (fh (if (eq split-type :vertical) (* h% h) h)))
          (update-frame c-frame fw fh x y)
          (ecase split-type
-           (:vertical (incf y fh))
+           (:vertical (decf y fh))
            (:horizontal (incf x fw)))))))
 
 ;;------------------------------------------------------------
 
 (defun change-frame-size (frame new-w new-h)
   (when (parent frame)
-    (let* ((children (loop for c :in (children (parent frame)) :for i :from 0
-                        :collect (cons i c)))
-           (to-index (position frame children :key #'first))
-           (is-last (= to-index (1- (length children))))
-           (from-index (if is-last (1- to-index) (1+ to-index))))
-      (assert (= 1 (abs (- to-index from-index))))
-      (let ((res (sort (append (subseq children 0 (min to-index from-index))
-                               (subseq children (max to-index from-index))
-                               (give-size new-w new-h
-                                          (elt children to-index)
-                                          (elt children from-index)))
-                       #'< :key #'car)))
-        (setf children (mapcar #'rest res)))
-      (assert (< (abs (- 100s0 (reduce #'+ children :key #'second))) 1))
-      (assert (< (abs (- 100s0 (reduce #'+ children :key #'third))) 1))))
+    (with-slots (children) (parent frame)
+      (let* ((to-pos (position frame children :key #'first))
+             (is-last (= to-pos (1- (length children))))
+             (from-pos (if is-last (1- to-pos) (1+ to-pos))))
+        (assert (= 1 (abs (- to-pos from-pos))))
+        (setf children
+              (append (subseq children 0 (min to-pos from-pos))
+                      (give-size new-w new-h
+                                 to-pos (elt children to-pos)
+                                 from-pos (elt children from-pos))
+                      (subseq children (max to-pos from-pos))))
+        (assert (< (abs (- 100s0 (reduce #'+ children :key #'second))) 1))
+        (assert (< (abs (- 100s0 (reduce #'+ children :key #'third))) 1)))))
   (update-frame-sizes))
 
-(defun give-size (w h to from)
-  (destructuring-bind (to-id to to-w to-h) to
-    (destructuring-bind (from-id from from-w from-h) from
+(defun give-size (w h to-pos to from-pos from)
+  (destructuring-bind (to to-w to-h) to
+    (destructuring-bind (from from-w from-h) from
       (let* ((to-w-dif (- w (max to-w *minimum-frame-size*)))
              (to-h-dif (- h (max to-h *minimum-frame-size*)))
              (proposed-to-w (- from-w to-w-dif))
              (proposed-to-h (- from-h to-h-dif))
              (to-w-dif (- to-w-dif (max proposed-to-w *minimum-frame-size*)))
              (to-h-dif (- to-h-dif (max proposed-to-h *minimum-frame-size*))))
-        (list (list to-id to (+ to-w to-w-dif) (+ to-h to-h-dif))
-              (list from-id from (- from-w to-w-dif) (+ from-h to-h-dif)))))))
+        (mapcar #'first
+                (sort (list (list to-pos to (+ to-w to-w-dif) (+ to-h to-h-dif))
+                            (list from-pos from (- from-w to-w-dif) (+ from-h to-h-dif)))
+                      #'< :key #'car))))))
 
 ;;------------------------------------------------------------
 
 (defparameter *group-count* 0)
-(defvar *current-group* :group-0)
-(defvar *last-group* nil)
-(defparameter *groups*
-  (let ((g (make-hash-table)))
-    (setf *groups* g
-          *group-count* 0)
-    (add-group t)))
+(defparameter *last-group* nil)
+(defparameter *groups* nil)
+(defvar *current-group* nil)
+
+(defun current-group ()
+  *current-group*)
 
 (defun add-group (&optional switch-to-group)
-  (let ((name (intern (format nil "GROUP-~s" (incf *group-count*)))))
-    (setf (gethash name *groups*) (make-frame))
+  (let* ((name (intern (format nil "GROUP-~s" (incf *group-count*))
+                      :keyword))
+         (frame (make-frame :group-id name)))
+    (setf (frame (children frame)) frame)
+    (setf (gethash name *groups*) frame)
     (when switch-to-group
       (switch-to-group name))
     name))
 
 (defun switch-to-group (id)
   (assert (group-exists-p id))
-  (push *current-group* *last-group*)
-  (setf *current-group* id))
+  (unless (eq id *current-group*)
+    (when *current-group*
+      (push *current-group* *last-group*))
+    (setf *current-group* id)
+    (%focus-some-buffer-in-frame (gethash id *groups*)))
+  id)
+
+(defun %focus-some-buffer-in-frame (frame)
+  "this is a hack but will work for now, ideally I guess we should
+store the focused buffer for each group. I will come back to this
+though as sooner or later it will start to annoy me"
+  (let ((buffer (%find-some-buffer frame)))
+    (%switch-to-buffer-obj (frame buffer) buffer)))
+
+(defun %find-some-buffer (frame)
+  (etypecase frame
+    (frame (with-slots (children) frame
+             (etypecase children
+               (list (find #'%find-some-buffer children))
+               (buffer children))))
+    (buffer frame)))
 
 (defun kill-group (id)
   (assert (group-exists-p id))
@@ -272,5 +378,12 @@
 
 (defun group-exists-p (id)
   (not (null (gethash id *groups*))))
+
+(defun init ()
+  (setf *group-count* 0)
+  (setf *groups* (make-hash-table))
+  (add-group t))
+
+(init)
 
 ;;------------------------------------------------------------
